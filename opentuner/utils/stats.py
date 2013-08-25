0cd2b8e651761c28eb7c86e5c6edb4805f2c5d8c
Jason Ansel <jansel@csail.mit.edu>
2013-08-24 22:14:00 -0400

    Bugfix for eps extensions in stats.py

--- opentuner/utils/stats.py
+++ opentuner/utils/stats.py
@@ -378,7 +378,7 @@ class StatsMain(object):
   def gnuplot_file(self, output_dir, prefix, plotcmd):
     with open(os.path.join(output_dir, prefix+'.gnuplot'), 'w') as fd:
       print >>fd, 'set terminal postscript eps enhanced color'
-      print >>fd, 'set output "%s"' % (prefix+'.pdf')
+      print >>fd, 'set output "%s"' % (prefix+'.eps')
       print >>fd, 'set ylabel "Execution Time (seconds)"'
       print >>fd, 'set xlabel "Autotuning Time (seconds)"'
       print >>fd, 'plot', ',\\\n'.join(plotcmd)
