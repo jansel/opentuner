e6eb0188e6c47c5650252743f6014090a874f279
Kevin Wu <sierangho@gmail.com>
2015-01-22 12:48:56 -0500

    added comments to composabletechniques.py

--- opentuner/search/composabletechniques.py
+++ opentuner/search/composabletechniques.py
@@ -25,26 +25,30 @@ class RandomThreeParentsComposableTechnique(ComposableSearchTechnique):
 
     # copy oldest
     cfg = self.manipulator.copy(population[0].config)
+
     shuffled_population = map(_.config, population[1:])
+    # mix in the global best configuration
     shuffled_population += ([self.get_global_best_configuration()]
                             * self.information_sharing)
-
     random.shuffle(shuffled_population)
 
+    # return oldest configuration +_3 other configurations
     return [cfg] + shuffled_population[0:3]
 
   def update_population(self, config, population):
+    # replace the oldest configuration if the new one is better.
     population.sort(key=_.timestamp)
     if self.lt(config, population[0].config):
       population[0].config = config
 
+    # mark that oldest configuration is updated
     population[0].touch
 
     return population
 
   def select_parameters(self, params):
     """
-    select a subset of parameters to operate on
+    randomly select a subset of parameters to operate on
     """
     ret_list = []
     random.shuffle(params)
@@ -56,9 +60,9 @@ class RandomThreeParentsComposableTechnique(ComposableSearchTechnique):
   def get_default_operator(self, param_type):
     return {'op_name': 'op4_set_linear', 'args': [1.0, self.use_f, -self.use_f], 'kwargs': {}}
 
+
 register(RandomThreeParentsComposableTechnique(name='ComposableDiffEvolution',
                                                  population_size=30))
-
 
 op_map = {}
 ComposableSearchTechnique.add_to_map(op_map,
@@ -67,8 +71,6 @@ ComposableSearchTechnique.add_to_map(op_map,
 ComposableSearchTechnique.add_to_map(op_map,
                                       "FloatArray",
                                       "op3_cross", strength=0.4)
-print op_map
 register(RandomThreeParentsComposableTechnique(name='ComposableDiffEvolutionCX',
                                                  operator_map=op_map,
                                                  population_size=30))
-
