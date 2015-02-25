47f67e4dd14273bb6abb42b9188e2d59dff9ccb1
Giuseppe <zingales@mit.edu>
2015-02-25 16:17:39 -0500

    fixed minor issue

--- examples/tutorials/mmm_block.cpp
+++ examples/tutorials/mmm_block.cpp
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <cstdlib>
 
+#define N 800
+
 int main(int argc, const char** argv)
 {
 
-  int n = BLOCK_SIZE * (n/BLOCK_SIZE);
-  int a[100][100];
-  int b[100][100];
-  int c[100][100];
+  int n = BLOCK_SIZE * (N/BLOCK_SIZE);
+  int a[N][N];
+  int b[N][N];
+  int c[N][N];
   int sum=0;
   for(int k1=0;k1<n;k1+=BLOCK_SIZE)
   {
